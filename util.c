e6320eecc9286a61915b1743f8f4ae7a76221dc6
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-12-15 12:30:27 +0300

    Update util.c

--- util.c
+++ util.c
@@ -1,27 +1,42 @@
-#include <stdio.h>
 #include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
 /**
- * _add -  adds the first two nodes of the stack
- * @stack: stack given by main
- * @line_cnt: line counter
+ * is_digit - checks if a string is a digit
+ * @string: string to check
  *
- * Return: void
+ * Return: 1 if success, 0 if not
  */
-void _add(stack_t **stack, unsigned int line_cnt)
+int is_digit(char *string)
 {
-	int result;
-
-	if (!stack || !*stack || !((*stack)->next))
+	if (!string || *string == '\0')
+		return (0);
+	if (*string == '-')
+		string++;
+	while (*string)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
-		exit(EXIT_FAILURE);
+		if (isdigit(*string) == 0)
+			return (0);
+		string++;
 	}
+	return (1);
+}
+/**
+ * isnumber - checks if a string is a number
+ * @str: provided string
+ *
+ * Return: 1 if the string is a number, else, 0.
+ */
+int isnumber(char *str)
+{
+	int i;
 
-	result = ((*stack)->next->n) + ((*stack)->n);
-	pop(stack, line_cnt); /*For top node*/
-	(*stack)->n = result;
+	if (!str)
+		return (0);
+
+	for (i = 0; str[i]; i++)
+		if (i < '0' || i > '9')
+			return (0);
+
+	return (1);
 }
